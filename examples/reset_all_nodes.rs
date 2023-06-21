9fbb2a438ac688aa8e58e4729d9ee5462d0cc8e7
Akihiko Miyadera <miyadera@smilerobotics.com>
2023-06-21 20:35:45 +0900

    Change the way to construct frame structs

--- examples/reset_all_nodes.rs
+++ examples/reset_all_nodes.rs
@@ -1,14 +1,17 @@
 use socketcan::{BlockingCan, CanSocket, Socket};
 
-use canopen_rs::frame::{CANOpenFrame, NMTCommand, NMTNodeControlAddress, NMTNodeControlFrame};
+use canopen_rs::frame::{CANOpenFrame, NMTCommand, NMTNodeControlAddress};
 
 const INTERFACE_NAME: &str = "can0";
 
 fn main() {
     let mut sock = CanSocket::open(INTERFACE_NAME).unwrap();
     sock.transmit(
-        &NMTNodeControlFrame::new(NMTCommand::ResetNode, NMTNodeControlAddress::AllNodes)
-            .to_socketcan_frame(),
+        &CANOpenFrame::new_nmt_node_control_frame(
+            NMTCommand::ResetNode,
+            NMTNodeControlAddress::AllNodes,
+        )
+        .into(),
     )
     .unwrap();
 }

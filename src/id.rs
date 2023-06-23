effbacca6b4934ea8b41e432788ba69337ad0080
Akihiko Miyadera <miyadera@smilerobotics.com>
2023-06-23 21:48:21 +0900

    Implement conversion from CAN ID to communication object

--- src/id.rs
+++ src/id.rs
@@ -46,6 +46,47 @@ pub enum CommunicationObject {
 }
 
 impl CommunicationObject {
+    fn raw_id_to_node_id(cob_id: u16) -> NodeID {
+        ((cob_id & 0x7F) as u8).try_into().unwrap()
+    }
+
+    fn new(id: u16) -> Result<Self> {
+        match id & !0x07FF {
+            0 => match id & 0b00000111_10000000 {
+                0x000 => match id {
+                    0 => Ok(CommunicationObject::NMTNodeControl),
+                    1 => Ok(CommunicationObject::GlobalFailsafeCommand),
+                    _ => Err(Error::InvalidCobId(id)),
+                },
+                0x080 => match id & 0x007F {
+                    0 => Ok(CommunicationObject::Sync),
+                    _ => Ok(CommunicationObject::Emergency(Self::raw_id_to_node_id(id))),
+                },
+                0x100 => Ok(CommunicationObject::TimeStamp),
+                0x180 => Ok(CommunicationObject::TxPDO1(Self::raw_id_to_node_id(id))),
+                0x200 => Ok(CommunicationObject::RxPDO1(Self::raw_id_to_node_id(id))),
+                0x280 => Ok(CommunicationObject::TxPDO2(Self::raw_id_to_node_id(id))),
+                0x300 => Ok(CommunicationObject::RxPDO2(Self::raw_id_to_node_id(id))),
+                0x380 => Ok(CommunicationObject::TxPDO3(Self::raw_id_to_node_id(id))),
+                0x400 => Ok(CommunicationObject::RxPDO3(Self::raw_id_to_node_id(id))),
+                0x480 => Ok(CommunicationObject::TxPDO4(Self::raw_id_to_node_id(id))),
+                0x500 => Ok(CommunicationObject::RxPDO4(Self::raw_id_to_node_id(id))),
+                0x580 => Ok(CommunicationObject::TxSDO(Self::raw_id_to_node_id(id))),
+                0x600 => Ok(CommunicationObject::RxSDO(Self::raw_id_to_node_id(id))),
+                0x700 => Ok(CommunicationObject::NMTNodeMonitoring(
+                    Self::raw_id_to_node_id(id),
+                )),
+                0x780 => match id {
+                    0x7E4 => Ok(CommunicationObject::TxLSS),
+                    0x7E5 => Ok(CommunicationObject::RxLSS),
+                    _ => Err(Error::InvalidCobId(id)),
+                },
+                _ => Err(Error::InvalidCobId(id)),
+            },
+            _ => Err(Error::InvalidCobId(id)),
+        }
+    }
+
     fn as_cob_id(&self) -> u16 {
         match self {
             CommunicationObject::NMTNodeControl => 0x000,
@@ -76,6 +117,16 @@ impl From<CommunicationObject> for socketcan::Id {
         // The `new` method could return `None` if the raw ID is greater than 0x7FF.
         // But `CommunicationObject::as_cob_id` method never returns such values,
         // because its inner node_id is limited.
+    }
+}
+
+impl TryFrom<socketcan::Id> for CommunicationObject {
+    type Error = Error;
+    fn try_from(id: socketcan::Id) -> Result<Self> {
+        match id {
+            socketcan::Id::Standard(id) => CommunicationObject::new(id.as_raw()),
+            socketcan::Id::Extended(_id) => Err(Error::CanFdNotSupported),
+        }
     }
 }
 
@@ -249,5 +300,166 @@ mod tests {
             id,
             socketcan::Id::Standard(socketcan::StandardId::new(0x7E5).unwrap())
         );
+    }
+
+    #[test]
+    fn test_new() {
+        let cob = CommunicationObject::new(0x000);
+        assert_eq!(cob, Ok(CommunicationObject::NMTNodeControl));
+        let cob = CommunicationObject::new(0x001);
+        assert_eq!(cob, Ok(CommunicationObject::GlobalFailsafeCommand));
+        let cob = CommunicationObject::new(0x080);
+        assert_eq!(cob, Ok(CommunicationObject::Sync));
+        let cob = CommunicationObject::new(0x81);
+        assert_eq!(
+            cob,
+            Ok(CommunicationObject::Emergency(1.try_into().unwrap()))
+        );
+        let cob = CommunicationObject::new(0x08F);
+        assert_eq!(
+            cob,
+            Ok(CommunicationObject::Emergency(15.try_into().unwrap()))
+        );
+        let cob = CommunicationObject::new(0x0FF);
+        assert_eq!(
+            cob,
+            Ok(CommunicationObject::Emergency(127.try_into().unwrap()))
+        );
+        let cob = CommunicationObject::new(0x100);
+        assert_eq!(cob, Ok(CommunicationObject::TimeStamp));
+        let cob = CommunicationObject::new(0x181);
+        assert_eq!(cob, Ok(CommunicationObject::TxPDO1(1.try_into().unwrap())));
+        let cob = CommunicationObject::new(0x18F);
+        assert_eq!(cob, Ok(CommunicationObject::TxPDO1(15.try_into().unwrap())));
+        let cob = CommunicationObject::new(0x1FF);
+        assert_eq!(
+            cob,
+            Ok(CommunicationObject::TxPDO1(127.try_into().unwrap()))
+        );
+        let cob = CommunicationObject::new(0x201);
+        assert_eq!(cob, Ok(CommunicationObject::RxPDO1(1.try_into().unwrap())));
+        let cob = CommunicationObject::new(0x20F);
+        assert_eq!(cob, Ok(CommunicationObject::RxPDO1(15.try_into().unwrap())));
+        let cob = CommunicationObject::new(0x27F);
+        assert_eq!(
+            cob,
+            Ok(CommunicationObject::RxPDO1(127.try_into().unwrap()))
+        );
+        let cob = CommunicationObject::new(0x281);
+        assert_eq!(cob, Ok(CommunicationObject::TxPDO2(1.try_into().unwrap())));
+        let cob = CommunicationObject::new(0x28F);
+        assert_eq!(cob, Ok(CommunicationObject::TxPDO2(15.try_into().unwrap())));
+        let cob = CommunicationObject::new(0x2FF);
+        assert_eq!(
+            cob,
+            Ok(CommunicationObject::TxPDO2(127.try_into().unwrap()))
+        );
+        let cob = CommunicationObject::new(0x301);
+        assert_eq!(cob, Ok(CommunicationObject::RxPDO2(1.try_into().unwrap())));
+        let cob = CommunicationObject::new(0x30F);
+        assert_eq!(cob, Ok(CommunicationObject::RxPDO2(15.try_into().unwrap())));
+        let cob = CommunicationObject::new(0x37F);
+        assert_eq!(
+            cob,
+            Ok(CommunicationObject::RxPDO2(127.try_into().unwrap()))
+        );
+        let cob = CommunicationObject::new(0x381);
+        assert_eq!(cob, Ok(CommunicationObject::TxPDO3(1.try_into().unwrap())));
+        let cob = CommunicationObject::new(0x38F);
+        assert_eq!(cob, Ok(CommunicationObject::TxPDO3(15.try_into().unwrap())));
+        let cob = CommunicationObject::new(0x3FF);
+        assert_eq!(
+            cob,
+            Ok(CommunicationObject::TxPDO3(127.try_into().unwrap()))
+        );
+        let cob = CommunicationObject::new(0x401);
+        assert_eq!(cob, Ok(CommunicationObject::RxPDO3(1.try_into().unwrap())));
+        let cob = CommunicationObject::new(0x40F);
+        assert_eq!(cob, Ok(CommunicationObject::RxPDO3(15.try_into().unwrap())));
+        let cob = CommunicationObject::new(0x47F);
+        assert_eq!(
+            cob,
+            Ok(CommunicationObject::RxPDO3(127.try_into().unwrap()))
+        );
+        let cob = CommunicationObject::new(0x481);
+        assert_eq!(cob, Ok(CommunicationObject::TxPDO4(1.try_into().unwrap())));
+        let cob = CommunicationObject::new(0x48F);
+        assert_eq!(cob, Ok(CommunicationObject::TxPDO4(15.try_into().unwrap())));
+        let cob = CommunicationObject::new(0x4FF);
+        assert_eq!(
+            cob,
+            Ok(CommunicationObject::TxPDO4(127.try_into().unwrap()))
+        );
+        let cob = CommunicationObject::new(0x501);
+        assert_eq!(cob, Ok(CommunicationObject::RxPDO4(1.try_into().unwrap())));
+        let cob = CommunicationObject::new(0x50F);
+        assert_eq!(cob, Ok(CommunicationObject::RxPDO4(15.try_into().unwrap())));
+        let cob = CommunicationObject::new(0x57F);
+        assert_eq!(
+            cob,
+            Ok(CommunicationObject::RxPDO4(127.try_into().unwrap()))
+        );
+        let cob = CommunicationObject::new(0x581);
+        assert_eq!(cob, Ok(CommunicationObject::TxSDO(1.try_into().unwrap())));
+        let cob = CommunicationObject::new(0x58F);
+        assert_eq!(cob, Ok(CommunicationObject::TxSDO(15.try_into().unwrap())));
+        let cob = CommunicationObject::new(0x5FF);
+        assert_eq!(cob, Ok(CommunicationObject::TxSDO(127.try_into().unwrap())));
+        let cob = CommunicationObject::new(0x601);
+        assert_eq!(cob, Ok(CommunicationObject::RxSDO(1.try_into().unwrap())));
+        let cob = CommunicationObject::new(0x60F);
+        assert_eq!(cob, Ok(CommunicationObject::RxSDO(15.try_into().unwrap())));
+        let cob = CommunicationObject::new(0x67F);
+        assert_eq!(cob, Ok(CommunicationObject::RxSDO(127.try_into().unwrap())));
+        let cob = CommunicationObject::new(0x701);
+        assert_eq!(
+            cob,
+            Ok(CommunicationObject::NMTNodeMonitoring(
+                1.try_into().unwrap()
+            ))
+        );
+        let cob = CommunicationObject::new(0x70F);
+        assert_eq!(
+            cob,
+            Ok(CommunicationObject::NMTNodeMonitoring(
+                15.try_into().unwrap()
+            ))
+        );
+        let cob = CommunicationObject::new(0x77F);
+        assert_eq!(
+            cob,
+            Ok(CommunicationObject::NMTNodeMonitoring(
+                127.try_into().unwrap()
+            ))
+        );
+        let cob = CommunicationObject::new(0x7E4);
+        assert_eq!(cob, Ok(CommunicationObject::TxLSS));
+        let cob = CommunicationObject::new(0x7E5);
+        assert_eq!(cob, Ok(CommunicationObject::RxLSS));
+    }
+
+    #[test]
+    fn test_socketcan_id_into_cob() {
+        let cob: Result<CommunicationObject> =
+            socketcan::Id::Standard(socketcan::StandardId::new(0x000).unwrap()).try_into();
+        assert_eq!(cob, Ok(CommunicationObject::NMTNodeControl));
+        let cob: Result<CommunicationObject> =
+            socketcan::Id::Standard(socketcan::StandardId::new(0x001).unwrap()).try_into();
+        assert_eq!(cob, Ok(CommunicationObject::GlobalFailsafeCommand));
+        let cob: Result<CommunicationObject> =
+            socketcan::Id::Standard(socketcan::StandardId::new(0x080).unwrap()).try_into();
+        assert_eq!(cob, Ok(CommunicationObject::Sync));
+        let cob: Result<CommunicationObject> =
+            socketcan::Id::Standard(socketcan::StandardId::new(0x081).unwrap()).try_into();
+        assert_eq!(
+            cob,
+            Ok(CommunicationObject::Emergency(1.try_into().unwrap()))
+        );
+        let cob: Result<CommunicationObject> =
+            socketcan::Id::Standard(socketcan::StandardId::new(0x67F).unwrap()).try_into();
+        assert_eq!(cob, Ok(CommunicationObject::RxSDO(127.try_into().unwrap())));
+        let cob: Result<CommunicationObject> =
+            socketcan::Id::Extended(socketcan::ExtendedId::new(0x0000).unwrap()).try_into();
+        assert_eq!(cob, Err(Error::CanFdNotSupported));
     }
 }

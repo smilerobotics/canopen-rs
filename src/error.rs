a4105384a2762737731592b01318a74e1f3c792b
Akihiko Miyadera <miyadera@smilerobotics.com>
2023-06-23 21:48:21 +0900

    Implement conversion from CAN ID to communication object

--- src/error.rs
+++ src/error.rs
@@ -4,6 +4,10 @@
 pub enum Error {
     #[error("Invalid Node ID ({})", .0)]
     InvalidNodeId(u8),
+    #[error("Invalid COB ID ({:03X})", .0)]
+    InvalidCobId(u16),
+    #[error("CAN-FD is not supported")]
+    CanFdNotSupported,
 }
 
 pub type Result<T> = std::result::Result<T, Error>;

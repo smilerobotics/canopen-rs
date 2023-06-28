4cf0d8ab2996ebfb33f6e957037755db2b42075f
Akihiko Miyadera <miyadera@smilerobotics.com>
2023-06-28 11:42:55 +0900

    Receive SDO result and show the type of the communication bject

--- examples/motor_sdo.rs
+++ examples/motor_sdo.rs
@@ -1,11 +1,10 @@
-use socketcan::{BlockingCan, CanSocket, Socket};
+use socketcan::{BlockingCan, CanSocket, EmbeddedFrame, Socket};
 
 use canopen_rs::frame::{CANOpenFrame, NMTCommand, NMTNodeControlAddress};
-use canopen_rs::id::NodeID;
+use canopen_rs::id::{CommunicationObject, NodeID};
 
 const INTERFACE_NAME: &str = "can0";
 const NODE_ID: u8 = 1;
-const INTERVAL_MILLIS: u64 = 100;
 
 fn main() {
     let node_id: NodeID = NodeID::new(NODE_ID).unwrap();
@@ -18,35 +17,49 @@ fn main() {
         .into(),
     )
     .unwrap();
-    std::thread::sleep(std::time::Duration::from_millis(1000));
+    let cob: CommunicationObject = sock.receive().unwrap().id().try_into().unwrap();
+    println!("received communication object: {:?}", cob);
 
     let frame = CANOpenFrame::new_sdo_write_frame(node_id, 0x6060, 0, &[3]);
     sock.transmit(&frame.into()).unwrap();
-    std::thread::sleep(std::time::Duration::from_millis(INTERVAL_MILLIS));
+    let received = sock.receive().unwrap();
+    let cob: CommunicationObject = received.id().try_into().unwrap();
+    println!("received communication object: {:?}", cob);
 
     let frame = CANOpenFrame::new_nmt_node_control_frame(
         NMTCommand::Operational,
         NMTNodeControlAddress::Node(node_id),
     );
     sock.transmit(&frame.into()).unwrap();
-    std::thread::sleep(std::time::Duration::from_millis(INTERVAL_MILLIS));
+    std::thread::sleep(std::time::Duration::from_millis(100));
 
     let frame =
         CANOpenFrame::new_sdo_write_frame(node_id, 0x6040, 0, &0b0001_0000_0110u16.to_le_bytes());
     sock.transmit(&frame.into()).unwrap();
-    std::thread::sleep(std::time::Duration::from_millis(INTERVAL_MILLIS));
+    let received = sock.receive().unwrap();
+    let cob: CommunicationObject = received.id().try_into().unwrap();
+    println!("received communication object: {:?}", cob);
+
     let frame =
         CANOpenFrame::new_sdo_write_frame(node_id, 0x6040, 0, &0b0001_0000_0111u16.to_le_bytes());
     sock.transmit(&frame.into()).unwrap();
-    std::thread::sleep(std::time::Duration::from_millis(INTERVAL_MILLIS));
+    let received = sock.receive().unwrap();
+    let cob: CommunicationObject = received.id().try_into().unwrap();
+    println!("received communication object: {:?}", cob);
+
     let frame =
         CANOpenFrame::new_sdo_write_frame(node_id, 0x6040, 0, &0b0001_0000_1111u16.to_le_bytes());
     sock.transmit(&frame.into()).unwrap();
-    std::thread::sleep(std::time::Duration::from_millis(INTERVAL_MILLIS));
+    let received = sock.receive().unwrap();
+    let cob: CommunicationObject = received.id().try_into().unwrap();
+    println!("received communication object: {:?}", cob);
+
     let frame =
         CANOpenFrame::new_sdo_write_frame(node_id, 0x6040, 0, &0b0000_0000_1111u16.to_le_bytes());
     sock.transmit(&frame.into()).unwrap();
-    std::thread::sleep(std::time::Duration::from_millis(INTERVAL_MILLIS));
+    let received = sock.receive().unwrap();
+    let cob: CommunicationObject = received.id().try_into().unwrap();
+    println!("received communication object: {:?}", cob);
 
     let mut str_velocity = String::new();
     loop {
@@ -57,6 +70,10 @@ fn main() {
             let frame =
                 CANOpenFrame::new_sdo_write_frame(node_id, 0x60FF, 0, &velocity.to_le_bytes());
             sock.transmit(&frame.into()).unwrap();
+            let received = sock.receive().unwrap();
+            let cob: CommunicationObject = received.id().try_into().unwrap();
+            println!("received communication object: {:?}", cob);
+
             let frame = CANOpenFrame::new_sdo_write_frame(
                 node_id,
                 0x6040,
@@ -64,10 +81,17 @@ fn main() {
                 &0b0000_0000_1111u16.to_le_bytes(),
             );
             sock.transmit(&frame.into()).unwrap();
+            let received = sock.receive().unwrap();
+            let cob: CommunicationObject = received.id().try_into().unwrap();
+            println!("received communication object: {:?}", cob);
         } else {
             let frame =
                 CANOpenFrame::new_sdo_write_frame(node_id, 0x6040, 0, &0x00u16.to_le_bytes());
             sock.transmit(&frame.into()).unwrap();
+            let received = sock.receive().unwrap();
+            let cob: CommunicationObject = received.id().try_into().unwrap();
+            println!("received communication object: {:?}", cob);
+
             break;
         }
         str_velocity.clear();

909029ac80a59eb289e4de15eb3c9e2c2cfe89c5
Akihiko Miyadera <miyadera@smilerobotics.com>
2023-06-30 18:58:41 +0900

    Implement NmtNodeMonitoringFrame

--- examples/sdo.rs
+++ examples/sdo.rs
@@ -1,7 +1,6 @@
-use socketcan::{BlockingCan, CanSocket, EmbeddedFrame, Socket};
+use socketcan::{BlockingCan, CanSocket, Socket};
 
 use canopen_rs::frame::{CanOpenFrame, NmtCommand, NmtNodeControlAddress};
-use canopen_rs::id::CommunicationObject;
 
 const INTERFACE_NAME: &str = "can0";
 const NODE_ID: u8 = 1;
@@ -17,8 +16,8 @@ fn main() {
     )
     .unwrap();
 
-    let cob: CommunicationObject = sock.receive().unwrap().id().try_into().unwrap();
-    println!("received communication object: {:?}", cob);
+    let frame: CanOpenFrame = sock.receive().unwrap().try_into().unwrap();
+    println!("received: {:?}", frame);
 
     sock.transmit(
         &CanOpenFrame::new_sdo_read_frame(NODE_ID.try_into().unwrap(), 0x1018, 2) // read `Product code`
